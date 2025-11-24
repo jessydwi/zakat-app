51a30e38c08255638e43b1991f14755220f85e5a
jessydwi <jessyydwiienjellinaa@gmail.com>
2025-11-24 13:55:36 +0700

    tambhan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        \App\Http\Middleware\TrustProxies::class;
+        // Register TrustProxies globally to ensure HTTPS detection behind proxy
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
         $middleware->web();
         $middleware->api();
 
