b3a4094ffb0c1c09a761f699a6d79257c4178782
jessydwi <jessyydwiienjellinaa@gmail.com>
2025-11-24 13:38:00 +0700

    tambahan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        \App\Http\Middleware\TrustProxies::class;
         $middleware->web();
         $middleware->api();
 
